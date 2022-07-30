b3012bd43ad4c47e6398eb503402c7ecd743157a
M Haidar Hanif <git@mhaidarhanif.com>
2022-07-30 17:23:59 +0700

    embed google form iframe

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -4,6 +4,18 @@ export default function Contact() {
       <div className="hero">
         <h2 className="intro">Let's book your discovery call</h2>
       </div>
+
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfaNjTKOA6BMZPTt8qFaCAOc-ycwIxuILUZnjauwmXOVyLTbQ/viewform?embedded=true"
+        className="iframe-contact-form"
+        width={640}
+        height={1400}
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Loading form…
+      </iframe>
     </div>
   );
 }
