00b18565ef5528f0ee684ac512661b150dfd5f03
berry <50803250+MatchaBear@users.noreply.github.com>
2022-07-30 17:46:17 +0800

    add hero and style some elements

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,13 +2,15 @@ export default function Home() {
   return (
     <div>
       <h1 className="logo">Antoinelle</h1>
-      <h2 className="intro">
-        Bespoke branding and showit website design for wedding pros, interior
-        designers, and passionate creatives
-      </h2>
-      <a href="/contact" className="cta-button">
-        Book Your Discovery Call
-      </a>
+      <div className="hero">
+        <h2 className="intro">
+          Bespoke branding and showit website design for wedding pros, interior
+          designers, and passionate creatives
+        </h2>
+        <a href="/contact" className="cta-button">
+          Book Your Discovery Call
+        </a>
+      </div>
       <img src="/assets/preview.jpeg" alt="preview" className="preview-image" />
     </div>
   );
