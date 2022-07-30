7614c4e24b8f552dee332cc48556b4f677b77d3e
berry <50803250+MatchaBear@users.noreply.github.com>
2022-07-30 17:28:16 +0800

    prepare style and font

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1>Antoinelle</h1>
-      <h2>
+      <h1 className="logo">Antoinelle</h1>
+      <h2 className="intro">
         Bespoke branding and showit website design for wedding pros, interior
         designers, and passionate creatives
       </h2>
-      <a href="/contact">Book Your Discovery Call</a>
-      <img src="/assets/preview.jpeg" alt="preview" />
+      <a href="/contact" className="cta-button">
+        Book Your Discovery Call
+      </a>
+      <img src="/assets/preview.jpeg" alt="preview" className="preview-image" />
     </div>
   );
 }
