032161cf45cb9a55d94c9e5aa7e7cdae966a7a62
berry <50803250+MatchaBear@users.noreply.github.com>
2022-07-30 17:20:46 +0800

    implement homepage content

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <div>
       <h1>Antoinelle</h1>
+      <h2>
+        Bespoke branding and showit website design for wedding pros, interior
+        designers, and passionate creatives
+      </h2>
+      <a href="/contact">Book Your Discovery Call</a>
+      <img src="/assets/preview.jpeg" alt="preview" />
     </div>
   );
 }
