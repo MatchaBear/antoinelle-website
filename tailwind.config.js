91dd0d493cc238d2a28b0a0d0922821a54a3187a
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2022-12-17 18:19:07 +0800

    cleanup tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,7 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      blue: "#1fb6ff",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      backgroundImage: {
-        "hero-pattern": "url('/assets/images/hero.jpg')",
-      },
-      spacing: {
-        128: "32rem",
-        144: "36rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-      maxHeight: {
-        128: "32rem",
-      },
-    },
+    extend: {},
     plugins: [],
   },
 };
