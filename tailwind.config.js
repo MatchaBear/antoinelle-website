14fdc005ec4331ef8efb6abca57ce9ce242903ff
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2022-12-18 19:04:10 +0800

    add custom color and border radius amount

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,20 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     fontFamily: {
-      'span': ['"Source Serif Pro"', 'serif']
+      'eaves': ['mrs-eaves-roman-lining', 'serif']
+    },
+    borderRadius: {
+      '6xl': '3rem',
+      '9xl': '4.5rem'
     },
     extend: {
       backgroundImage: {
         'hero': "url('/assets/images/hero.jpg')"
-      }
+      },
+      colors: {
+        'beige': '#F3EFE6',
+        'button-text': '#946B4D'
+      },
     },
   },
   plugins: [],
